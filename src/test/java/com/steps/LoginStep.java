e46d6f53a79dba91a1df6b466ba9fa82268630c7
Cree <Cree@cristians-mbp.attlocal.net>
2024-04-03 19:57:13 -0700

    msg

--- src/test/java/com/steps/LoginStep.java
+++ src/test/java/com/steps/LoginStep.java
@@ -5,12 +5,12 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.github.bonigarcia.wdm.WebDriverManager;
-import junit.framework.Assert;
 
 public class LoginStep {
 	
@@ -19,8 +19,8 @@ public class LoginStep {
 	
 	@Given("User navigates to login page {string}")
 	public void user_navigates_to_login_page(String url) throws InterruptedException {
-		WebDriverManager.firefoxdriver().setup();
-		driver = new FirefoxDriver();
+		WebDriverManager.safaridriver().setup();
+		driver = new SafariDriver();
 		Thread.sleep(2000);
 		driver.manage().window().maximize();
 		driver.get(url);
